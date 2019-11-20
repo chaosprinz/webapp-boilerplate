275a074cfa294f3b27e2a944262db75468f04b62
chaosprinz76 <chaosprinz76@googlemail.com>
2019-11-20 13:38:45 +0100

    implementing normalize.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom"
 import {connect, Provider} from 'react-redux'
 import store from './store'
 
+import 'normalize.css'
+
 import App from "./containers/app"
 
 ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [{loader: 'style-loader'}, {loader: 'css-loader'}]
       }
     ]
   },
